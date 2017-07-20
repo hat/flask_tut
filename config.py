b1cb5365cf6ffe5109d5fdf459b11ad09587b248
Tony Hendrick <tony@staff.42.us.org>
2017-07-20 09:37:56 -0700

    Trying to make live

--- config.py
+++ config.py
@@ -1,6 +1,26 @@
 #DATABASE
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+# import os
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+__author__ = 'tony'
+
+
+class Config():
+    APP_FOLDER = '/home/webmaster/security/'
+    DATABASE_HOST = 'security-host.42.us.org'
+    DATABASE_NAME = 'main'
+    DATABASE_USER = 'security'
+    DATABASE_PASSWORD = 'hjd8fn239dj29dj92dj29jd29dj2'
+    UPLOAD_FOLDER = '/home/webmaster/nostromo/app/static/uploads/'
+    CRYPT_KEY = 'gBenh6sQxtQsYfU2iqzKhSq3e1tWbA'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    SQLALCHEMY_ECHO=False
+    SQLALCHEMY_DATABASE_URI = 'postgres://%s:%s@%s/%s' % (DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_NAME)
+    SQLALCHEMY_BINDS = {
+        'database': 'postgres://%s:%s@%s/database' % (DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST),
+        'main': 'postgres://%s:%s@%s/main' % (DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST),
+        'stats': 'postgres://%s:%s@bigbrother-sql.42.us.org/stats' % ('stats', 'fdASmn301OOKdmnv32c563VF')
+    }
---
+++ server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import tornado
+from tornado.ioloop import IOLoop
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.web import FallbackHandler, RequestHandler, Application
+
+class MainHandler(RequestHandler):
+  def get(self):
+    self.write("Powered by Tornado")
+
+tr = WSGIContainer(backend_app)
+
+application = Application([
+    (r"/tornado", MainHandler),
+    (r".*", FallbackHandler, dict(fallback=tr)),
+    ], cookie_secret="SECRET_DE_FOU_2349882034823")
+
+if __name__ == "__main__":
+  server = HTTPServer(application)
+  server.bind(80)
+  server.start(0)
+  IOLoop.current().start()
