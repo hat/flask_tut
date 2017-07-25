68ad170481727d3becdba0ee0aa381a0ee8760c8
root <root@template-debian.42.us.org>
2017-07-25 15:43:24 -0700

    Merge

--- server.py
+++ server.py
@@ -26,7 +26,7 @@ import tornado
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-from yourapplication import app
+from app import app
 
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(5000)
